4eaa2e6da4cf240998c4673f5877411283615d8f
S-Hend12 <52982548+S-Hend12@users.noreply.github.com>
2020-02-21 17:05:39 -0500

    Update RAC_CVI_Consumer_Check_v2_Report_Script.r

--- RAC_CVI_Consumer_Check_v2_Report_Script.r
+++ RAC_CVI_Consumer_Check_v2_Report_Script.r
@@ -8,8 +8,23 @@ library(openxlsx)
 
 #--------------- SETUP ---------------#
 
-# reads excel file
-df <- read_excel("RAC CVI Consumer Check v2 02-15-2020.xlsx", sheet = "Sheet1")
+# directory paths
+mainDir <- "C:/Users/shenderson/Desktop"
+subDir <- "RAC_CVI_Consumer_Check_v2_Exports"
+
+# sets working directory to sub path - this is where stuff is exported
+setwd(mainDir)
+
+#Check if the sub directory folder exists in the current directory, if not then creates it
+ifelse(!dir.exists(subDir), dir.create(subDir), "Export directory already exists")
+
+setwd(file.path(mainDir, subDir))
+
+# reads excel file - opens file browser window
+df <- read_excel(file.choose())
+
+# reads excel file - hardcode path - optional
+#df <- read_excel("RAC CVI Consumer Check v2 02-21-2020.xlsx", sheet = "Sheet1")
 
 # cleans up column names
 names(df) <- make_clean_names(names(df))
@@ -29,7 +44,7 @@ df <- df %>%
       exception_reason == "existing wearer" ~ "PREV TAGGED",
       patient_first_name == previous_patient_first_name ~ "TAG",
       patient_first_name != previous_patient_first_name ~ "DIFFERENT PATIENT"
-))
+    ))
 
 # re-orders columns -> puts notes at start
 df <- df %>%
@@ -42,11 +57,11 @@ View(df)
 df_exceptions <- df %>%
   filter(notes == "TAG") %>%
   select(transaction_number) %>%
-    mutate(
-      Exception = "TRUE",
-      'Exception Reason' = "existing wearer",
-      Client = "CVI"
-)
+  mutate(
+    Exception = "TRUE",
+    'Exception Reason' = "existing wearer",
+    Client = "CVI"
+  )
 
 # renames transaction_number to be used for app support tool
 df_exceptions <- df_exceptions %>%
@@ -81,4 +96,4 @@ conditionalFormatting(wb, "Data", cols = 1:52, rows = 1:100, type = "expression"
 conditionalFormatting(wb, "Data", cols = 1:52, rows = 1:100, type = "expression", rule = '$A1="DIFFERENT PATIENT"', style = greenStyle)
 
 # saves excel workbook
-saveWorkbook(wb, "RAC CVI Consumer Check v2 02-15-2020 TESTING12.xlsx")
+saveWorkbook(wb, "RAC CVI Consumer Check v2 02-21-2020 - BUILT.xlsx")
