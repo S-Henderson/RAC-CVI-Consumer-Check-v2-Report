39dd484a6a099791cbfe819a8a8444766e0eac68
S-Hend12 <52982548+S-Hend12@users.noreply.github.com>
2020-03-12 16:02:24 -0400

    Update RAC_CVI_Consumer_Check_v2_Report_Script.r

--- RAC_CVI_Consumer_Check_v2_Report_Script.r
+++ RAC_CVI_Consumer_Check_v2_Report_Script.r
@@ -12,25 +12,17 @@ print("Script Starting")
 
 # directory paths
 Desktop <- file.path(Sys.getenv("USERPROFILE"),"Desktop")
-subDir <- "RAC_CVI_Consumer_Check_v2_Exports"
+Export_Directory <- "RAC_CVI_Consumer_Check_v2_Exports"
 
-set_directory_paths <- function(directory) {
-  setwd(Desktop)
+#set directory to named export folder
+set_directory_paths <- function(mainDir, subDir) {
+  setwd(mainDir)
     ifelse(!dir.exists(subDir), dir.create(subDir), "Export directory already exists")
-      setwd(file.path(Desktop, subDir))
+      setwd(file.path(mainDir, subDir))
         print(paste0("Current Working Directory is ", getwd()))
 }
 
-set_directory_paths(Desktop)
-
-# # sets working directory to user desktop to add subfolder
-# setwd(mainDir)
-# 
-# #Check if the sub directory folder exists in the current directory, if not then creates it
-# ifelse(!dir.exists(subDir), dir.create(subDir), "Export directory already exists")
-# 
-# # sets working directory to sub path - this is where stuff is exported/saved
-# setwd(file.path(mainDir, subDir))
+set_directory_paths(Desktop, Export_Directory)
 
 # reads excel file - opens file browser window
 df <- read_excel(file.choose())
@@ -39,7 +31,11 @@ df <- read_excel(file.choose())
 #df <- read_excel("RAC CVI Consumer Check v2 02-21-2020.xlsx", sheet = "Sheet1")
 
 # cleans up column names
-names(df) <- make_clean_names(names(df))
+clean_headers <- function(df) {
+  make_clean_names(names(df))
+}
+
+names(df) <- clean_headers(df)
 
 #--------------- PREP REPORT ---------------#
 
@@ -47,52 +43,62 @@ names(df) <- make_clean_names(names(df))
 df <- df %>%
   distinct(transaction_number, .keep_all = TRUE)
 
-# adds notes column -> calculates values based on case_when
-df <- df %>%
-  mutate(
-    notes = case_when(
-      previous_claim_status == "Invalid Submission" ~ "INVALID",
-      is_blackhawk == "TRUE" ~ "BH TAG",
-      # if exception_reason is not blank - accounts for mutiple exception reasons
-      !is.na(exception_reason) ~ "PREV TAGGED",
-      # TAG name matches -> report pulls same last name so just check first names for match
-      patient_first_name == previous_patient_first_name ~ "TAG",
-      patient_first_name != previous_patient_first_name ~ "DIFFERENT PATIENT"
-    ))
+# adds Raction column
+Raction <- function(df) {
+  df <- df %>%
+    mutate(
+      'Raction' = case_when(
+        is_blackhawk == "TRUE" ~ "BH TAG",
+        previous_claim_status == "Invalid Submission" ~ "IS",
+        # if exception_reason is not blank - accounts for mutiple exception reasons
+        !is.na(exception_reason) ~ "PREV TAG",
+        # TAG name matches -> report pulls same last name so just check first names for match
+        patient_first_name == previous_patient_first_name ~ "TAG",
+        patient_first_name != previous_patient_first_name ~ "DIFFERENT PATIENT"
+      ))
+}
+
+df <- Raction(df)
 
 # adds patient first name match column -> mostly for audit check - following process doc instructions
-df <- df %>%
-  mutate(
-    patient_first_name_match = case_when(
-      patient_first_name == previous_patient_first_name ~ "TRUE",
-      patient_first_name != previous_patient_first_name ~ "FALSE"
-    ))
+patient_name_match <- function(df) {
+  df <- df %>%
+    mutate(
+      patient_first_name_match = case_when(
+        patient_first_name == previous_patient_first_name ~ "TRUE",
+        patient_first_name != previous_patient_first_name ~ "FALSE"
+      ))
+}
+
+df <- patient_name_match(df)
+
+#--------------- ORDER OF COLUMNS ---------------#
 
 # re-orders columns -> puts notes at start -> adds patient name match after patient names
 df <- df %>%
-  select(notes, 1:38, patient_first_name_match, everything())
-
-# view dataframe in RStudio
-##View(df)
+  select('Raction', 1:38, patient_first_name_match, everything())
 
 #--------------- BUILD EXCEPTIONS FILE ---------------#
 
 # build exceptions file to send to app support
-df_exceptions <- df %>%
-  filter(notes == "TAG") %>%
-  select(transaction_number) %>%
-  mutate(
-    Exception = "TRUE",
-    'Exception Reason' = "existing wearer",
-    Client = "CVI"
-  )
+build_exceptions <- function(df) {
+  df <- df %>%
+    filter('Raction' == "TAG"
+    ) %>%
+    select(transaction_number
+    ) %>%
+    mutate(
+      Exception = "TRUE",
+      'Exception Reason' = "existing wearer",
+      Client = "CVI"
+    )
+}
 
-# renames transaction_number to be used for app support tool
+df_exceptions <- build_exceptions(df)
+
+# renames transaction_number header to be used for app support tool
 df_exceptions <- df_exceptions %>%
   rename(Transaction = transaction_number)
-
-# view exception dataframe in RStudio
-##View(df_exceptions)
 
 #--------------- EXPORT EXCEPTIONS FILE ---------------#
 
@@ -128,9 +134,12 @@ greenStyle <- createStyle(fontColour = "#006100", bgFill = "#C6EFCE")
 writeData(wb, "Data", x = df)
 
 # conditional formatting rules to highlight excel rows based on notes value -> limit to 100 rows -> issues doing dynamic range for row
+# main rules
 conditionalFormatting(wb, "Data", cols = 1:52, rows = 1:100, type = "expression", rule = '$A1="TAG"', style = redStyle)
-conditionalFormatting(wb, "Data", cols = 1:52, rows = 1:100, type = "expression", rule = '$A1="PREV TAGGED"', style = yellowStyle)
+conditionalFormatting(wb, "Data", cols = 1:52, rows = 1:100, type = "expression", rule = '$A1="PREV TAG"', style = yellowStyle)
 conditionalFormatting(wb, "Data", cols = 1:52, rows = 1:100, type = "expression", rule = '$A1="DIFFERENT PATIENT"', style = greenStyle)
+# misc rules
+conditionalFormatting(wb, "Data", cols = 1:52, rows = 1:100, type = "expression", rule = '$A1="IS"', style = greenStyle)
 
 # built report filename -> xlsx format to allow conditional formatting
 built_report_filename_xlsx <- paste0("Copy of RAC CVI Consumer Check v2 ", format(Sys.Date(), "%m-%d-%Y"), ".xlsx")
